bd61c3c8489d208ebcd31bce1812262cbc9a2183
unknown <ravikiranjayanthi5555@gmail.com>
2025-06-20 18:08:22 +0530

    firstUI

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+
+import 'preline';
+import './style.css';
+
+  document.getElementById('helloFromJs').innerHTML = `
+  <div>
+   <h1>wassup</h1>
+  </div>
+`
+
+
+
+setupCounter(document.querySelector('#counter'))
